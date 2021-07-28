bbb4910eb4d5455c276c2721931e1d0b139f8fe5
Khalid <kjharbi@hotmail.com>
2021-07-28 21:16:36 +0300

    bitcoin-ticker app

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,13 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'price_screen.dart';
+
+const apiKey = 'C3103734-B2EE-4340-B6F5-5E310BD7E628';
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const bitcoinAverageURL =
+    'https://apiv2.bitcoinaverage.com/indices/global/ticker';
+
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -27,5 +37,28 @@ const List<String> cryptoList = [
   'ETH',
   'LTC',
 ];
+class CoinData {
 
-class CoinData {}
+
+
+Future getCoinData(String selectedCurrency) async {
+
+  String requestURL = '$coinAPIURL/BTC/$selectedCurrency?apikey=$apiKey';
+
+  http.Response response = await http.get(requestURL);
+
+  if (response.statusCode == 200) {
+    String decodedData  = response.body;
+
+    var lastPrice  = jsonDecode(decodedData)['rate'];
+
+    return lastPrice.toStringAsFixed(0);
+  }else{
+    print(requestURL);
+    print(response.statusCode);
+  }
+}
+
+
+
+}
